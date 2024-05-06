722d9c42abc8ffa85e9ebdd28d7c4e1d3c4598f2
Paula Berggren <pbb16a@acu.edu>
2024-05-06 16:29:18 -0400

    Initial Commit

---
+++ DoomStroll/DoomStrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoomStrollApp.swift
+//  DoomStroll
+//
+//  Created by Paula Berggren on 5/6/24.
+//
+
+import SwiftUI
+
+@main
+struct DoomStrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
